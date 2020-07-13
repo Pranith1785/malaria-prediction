f0b666a9c346e3914147cb0272bf18d286c20507
Pranith Kumar <pranithgourishetty@gmail.com>
2020-07-13 19:45:33 +0530

    initial setup

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/runapp',methods=['POST'])
+def runapp():
+
+    
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
+
